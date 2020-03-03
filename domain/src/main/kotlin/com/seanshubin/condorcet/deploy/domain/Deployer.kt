29dad369091af8d8ce28ab00e57ec9912a6fcc5f
Sean Shubin <sshubin@cj.com>
2020-03-02 16:47:32 -0800

    fixed duplicate name problem

--- domain/src/main/kotlin/com/seanshubin/condorcet/deploy/domain/Deployer.kt
+++ domain/src/main/kotlin/com/seanshubin/condorcet/deploy/domain/Deployer.kt
@@ -1,11 +1,25 @@
 package com.seanshubin.condorcet.deploy.domain
 
 import software.amazon.awscdk.core.App
+import software.amazon.awscdk.core.Stack
+import software.amazon.awscdk.services.ec2.InstanceClass
+import software.amazon.awscdk.services.ec2.InstanceSize
+import software.amazon.awscdk.services.ec2.InstanceType
+import software.amazon.awscdk.services.ec2.Vpc
+import software.amazon.awscdk.services.rds.DatabaseInstance
+import software.amazon.awscdk.services.rds.DatabaseInstanceEngine
+import software.amazon.awscdk.services.rds.DatabaseInstanceProps
 
 class Deployer : Runnable {
     override fun run() {
         val app = App()
-        CondorcetStack(app, GlobalConstants.condorcetStackName)
+        val stack = Stack(app, GlobalConstants.condorcetStackName)
+        val vpc = Vpc.Builder.create(stack, GlobalConstants.vpcName).build()
+        val instanceClass = InstanceType.of(
+            InstanceClass.STANDARD3,
+            InstanceSize.NANO)
+        val databaseInstanceProps = DatabaseInstanceProps.builder().masterUsername(GlobalConstants.databaseMasterUsername).engine(DatabaseInstanceEngine.MYSQL).instanceClass(instanceClass).vpc(vpc).build()
+        DatabaseInstance(stack, GlobalConstants.databaseName, databaseInstanceProps)
         app.synth()
     }
 }
--- domain/src/main/kotlin/com/seanshubin/condorcet/deploy/domain/GlobalConstants.kt
+++ domain/src/main/kotlin/com/seanshubin/condorcet/deploy/domain/GlobalConstants.kt
@@ -2,5 +2,7 @@ package com.seanshubin.condorcet.deploy.domain
 
 object GlobalConstants {
     val databaseMasterUsername = "master-username";
+    val databaseName = "database";
     val condorcetStackName = "condorcet-stack"
+    val vpcName = "vpc"
 }
